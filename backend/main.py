f4f06f570e02dcb8229f67a6db3a030e97ebf6d5
Evoleaves <enlacruzdepiedra@gmail.com>
2025-05-20 19:59:11 +0200

    Add files via upload

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+
+from fastapi import FastAPI
+from auth import app as auth_app  # Reutilizamos la app del archivo auth.py
+
+app = FastAPI(title="Cannacore API")
+
+# Montamos la app de autenticación en la raíz
+app.mount("/", auth_app)
